e23b0eaf39c5b2d101ab78b642eabb6772d75490
minhkakart <nguyenminh2732@gmail.com>
2024-11-15 17:45:10 +0700

    fix

--- Program.cs
+++ Program.cs
@@ -48,11 +48,11 @@ builder.Services.AddAppService();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+app.UseSwagger();
+app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
 app.UseCors("CorsPolicy");
